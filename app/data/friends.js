20275b9d4b8a3cdd6928944602dca7060182f0a7
Throwback74 <corey.slade@gmail.com>
2018-07-21 22:13:57 -0700

    working app push

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,63 @@
 //Friends Data 
 var friendsArray = [ {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Mark",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/589228/589228_v9_ba.jpg",
   "scores":[
       5,
       1,
       4,
-      4,
+      2,
+      5,
+      5,
+      5,
+      5,
+      1,
+      1
+    ]
+},{
+  "name":"Jeff",
+  "photo":"https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg",
+  "scores":[
+      5,
+      5,
+      5,
+      1,
+      5,
+      5,
+      1,
+      1,
+      1,
+      5
+    ]
+},{
+  "name":"Bill",
+  "photo":"https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg",
+  "scores":[
+      5,
+      3,
       5,
       1,
       2,
       5,
-      4,
+      5,
+      1,
+      5,
       1
+    ]
+},{
+  "name":"Elon",
+  "photo":"https://thenypost.files.wordpress.com/2018/07/tesla-debt_1.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+  "scores":[
+      5,
+      1,
+      2,
+      1,
+      1,
+      5,
+      5,
+      1,
+      1,
+      5
     ]
 }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,92 @@
-var friendsData = require("../data/friends");
+var path = require("path");
 
-var bodyParser = require('body-parser');
-var express = require("express");
-var app = express();
+var friends = require("../data/friends.js");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// var bodyParser = require('body-parser');
+// var express = require("express");
+// var app = express();
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+    res.json(friends);
   });
-};
+
 
   app.post("/api/friends", function(req, res) {
-    console.log("Log of data being pushed to friendsData: " + req.body);
-    friendsData.push(req.body);
-    res.json(true);
-    if(error){
-      console.log(error);
+    var userInput = req.body;
+    console.log("Log of data being pushed to friendsData: " + userInput);
+    var userScores = userInput.scores;
+    // req.body.scores = userScores;
+    // console.log(userScores);
+    
+    // friends.push(userInput);
+    
+    
+    console.log(userScores);
+    console.log(friends);
+    var match = "";
+    var matchedFriend = "";
+    var matchedImage = "";
+    var diffObj = {
+      name: ""  ,
+      image: "",
+      difference: 0
+    };
+
+    var diff = 0;
+    var diffArray = [];
+    // var matching = function(diffArray){
+    for(var i = 0; i < friends.length; i++){
+      
+    
+      for(var j = 0; j < userScores.length; j++){
+        diff = Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
+        // diff += parseInt(friends[i].scores[j]) - parseInt(userScores[j]);
+        // console.log("Pre Math.abs: " + diff);
+        // diff = Math.abs(diff);
+        console.log("Post Math.abs: " + diff);
+        console.log("Testing Math.abs: " + Math.abs(-3));
+        console.log("My diff equation: " +Math.abs((parseInt(friends[i].scores[j]) - parseInt(userScores[j]))));
+        diffObj.difference = diff;
+        diffObj.name = friends[i].name;
+        diffObj.image = friends[i].photo;
+        userScores[j] = parseInt(userScores[j]);
+        
+      }
+        // diffArray.push(diff);
+        diffArray.push({
+          name: diffObj.name,
+          image: diffObj.image,
+          difference: diffObj.difference
+        });
+        console.log(diffArray);
+        req.body.scores = userScores;
+        
     }
+    console.log(diffArray);
+  diffArray.sort(function(a, b){return a.difference - b.difference});
+  
+  match = diffArray[0];
+  matchedFriend = match.name;
+  matchedImage = match.image;
+  
+  friends.push(userInput);
+
+
+  console.log(userScores);
+  console.log(friends);
+console.log(diffArray);
+    // res.json(userInput + " User's Scores: " + userScores + " Match: " + match + " Match name: " + matchedFriend + " matched Photo: " + matchedImage);
+    res.json({
+      name: matchedFriend,
+      photo: matchedImage, 
+      data: match
+    });
+
+    
   });
+};
