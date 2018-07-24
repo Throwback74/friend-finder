e07f35955392beccb3b24a306072b20687fdd38e
Throwback74 <corey.slade@gmail.com>
2018-07-23 18:51:01 -0700

    updated with easter egg

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,91 +2,73 @@ var path = require("path");
 
 var friends = require("../data/friends.js");
 
-// var bodyParser = require('body-parser');
-// var express = require("express");
-// var app = express();
+module.exports = function (app) {
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-module.exports = function(app) {
-
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
-
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     var userInput = req.body;
-    console.log("Log of data being pushed to friendsData: " + userInput);
     var userScores = userInput.scores;
-    // req.body.scores = userScores;
-    // console.log(userScores);
-    
-    // friends.push(userInput);
-    
-    
-    console.log(userScores);
-    console.log(friends);
+    // var rickRoll = "../data/assets/video.mp4";
+    // var rickRoll2 = "../data/assets/song.mp3";
+
+    // var rickRoll3 = `<iframe width="100%" height="100" scrolling="no" frameborder="no" allow="autoplay" src="https://play.google.com/music/m/Tazi6x7hjg23a2cdkm7prn7bqyy?t=Never_Gonna_Give_You_Up_-_Rick_Astley"></iframe>`;
+
+    var rickRoll4 = `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&loop=1&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+
     var match = "";
     var matchedFriend = "";
     var matchedImage = "";
-    var diffObj = {
-      name: ""  ,
-      image: "",
-      difference: 0
-    };
-
-    var diff = 0;
-    var diffArray = [];
-    // var matching = function(diffArray){
-    for(var i = 0; i < friends.length; i++){
-      
     
-      for(var j = 0; j < userScores.length; j++){
+    var diffArray = [];
+
+    for (var i = 0; i < friends.length; i++) {
+      var diff = 0;
+      var diffObj = {
+        name: "",
+        image: "",
+        difference: 0
+      };
+      for (var j = 0; j < userScores.length; j++) {
         diff = Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
-        // diff += parseInt(friends[i].scores[j]) - parseInt(userScores[j]);
-        // console.log("Pre Math.abs: " + diff);
-        // diff = Math.abs(diff);
-        console.log("Post Math.abs: " + diff);
-        console.log("Testing Math.abs: " + Math.abs(-3));
-        console.log("My diff equation: " +Math.abs((parseInt(friends[i].scores[j]) - parseInt(userScores[j]))));
-        diffObj.difference = diff;
+        diffObj.difference += diff;
         diffObj.name = friends[i].name;
         diffObj.image = friends[i].photo;
         userScores[j] = parseInt(userScores[j]);
-        
       }
-        // diffArray.push(diff);
-        diffArray.push({
-          name: diffObj.name,
-          image: diffObj.image,
-          difference: diffObj.difference
-        });
-        console.log(diffArray);
-        req.body.scores = userScores;
-        
+      diffArray.push({
+        name: diffObj.name,
+        image: diffObj.image,
+        difference: diffObj.difference
+      });
+      req.body.scores = userScores;
     }
+    diffArray.sort(function (a, b) {
+      return a.difference - b.difference
+    });
+    match = diffArray[0];
+    matchedFriend = match.name;
+    matchedImage = match.image;
+
+    friends.push(userInput);
+
     console.log(diffArray);
-  diffArray.sort(function(a, b){return a.difference - b.difference});
-  
-  match = diffArray[0];
-  matchedFriend = match.name;
-  matchedImage = match.image;
-  
-  friends.push(userInput);
-
-
-  console.log(userScores);
-  console.log(friends);
-console.log(diffArray);
-    // res.json(userInput + " User's Scores: " + userScores + " Match: " + match + " Match name: " + matchedFriend + " matched Photo: " + matchedImage);
+    if(userInput.name === "Travis") {
+      res.json({
+        name: "Rick Astley",
+        photo: "https://media.giphy.com/media/a6OnFHzHgCU1O/giphy.gif",
+        song: rickRoll4,
+        astley: true
+      });
+    }else {
     res.json({
       name: matchedFriend,
-      photo: matchedImage, 
-      data: match
+      photo: matchedImage,
+      data: match,
+      astley: false
     });
-
-    
+  }
   });
 };
